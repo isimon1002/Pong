accfccf8c09fcfc3ccac80f81fd65029d46b204e
Isaac Simon <ijsimon@sbcglobal.net>
2017-10-09 14:38:52 -0500

    blocpong with my content

--- blocpong.js
+++ blocpong.js
@@ -4,22 +4,22 @@ var animate = window.requestAnimationFrame ||
   function(callback) { window.setTimeout(callback, 100/60) };
 
 var canvas = document.createElement('canvas');
-var width =  400;
+var width =  1000;
 var height = 600;
 canvas.width = width;
 canvas.height = height;
-var context = canvas.getContext('2d');
+var table = canvas.getContext('2d');
 
 window.onload = function() {
-  document.body.appendChild(canvas);
-  animate(step);
+    document.body.appendChild(canvas);
+    animate(step);
 };
 
-//step function
-var step = function() {
-  update();
-  render();
-  animate(step);
+
+function step(){
+    update();
+    render();
+    animate(step);
 };
 
 //update canvas
@@ -30,10 +30,10 @@ var update = function() {
 Player.prototype.update = function() {
   for(var key in keysDown) {
     var value = Number(key);
-    if(value == 37) { // left arrow key identifier
-      this.paddle.move(-4, 0);
-    } else if (value == 39) { // right arrow key identifier
-      this.paddle.move(4, 0);
+    if(value == 38) { // up arrow key identifier
+      this.paddle.move(0, -4);
+    } else if (value == 40) { // down arrow key identifier
+      this.paddle.move(0, 4);
     } else {
       this.paddle.move(0, 0);
     }
@@ -45,81 +45,73 @@ Paddle.prototype.move = function(x, y) {
   this.y += y;
   this.x_speed = x;
   this.y_speed = y;
-  if(this.x < 0) { //left side of canvas
+  if(this.y < 0) { //top side of canvas
     this.x = 0;
     this.x_speed = 0;
-  } else if (this.x + this.width > 400) { //right side of canvas
+  } else if (this.y + this.height > 600) { //bottom side of canvas
     this.x = 0;
-    this.x = 400 - this.width;
+    this.x = 600 - this.width;
     this.x_speed = 0;
   }
 }
 
-
 var player = new Player();
 var computer = new Computer();
-var ball = new Ball(200, 300);
+var ball = new Ball(675, 300);
 
-//render canvas elements
+
 var render = function() {
-  context.fillStyle = "black";
-  context.fillRect(0, 0, width, height);
+  table.rect(0, 0, width, height);
+  table.stroke();
   player.render();
   computer.render();
+  ball.render();
 };
 
-//paddle features
-function Paddle(x, y, width, height) {
+function Paddle(x, y) {
   this.x = x;
   this.y = y;
-  this.width = width;
-  this.height = height;
-  this.x_speed = 0;
-  this.y_speed = 0;
+  this.width = 10;
+  this.height = 50;
+  this.ySpeed = 0;
+  this.xSpeed = 0;
 }
 
-//paddle render
 Paddle.prototype.render = function() {
-  context.fillRect(this.x, this.y, this.width, this.height);
+  table.fillRect(this.x, this.y, this.width, this.height);
 };
 
-//player paddle
+
 function Player() {
-  this.paddle = new Paddle(175, 580, 50, 10);
+  this.paddle = new Paddle(990, 300, 10, 50);
 }
 
-//computer paddle
 function Computer() {
-  this.paddle = new Paddle(175, 10, 50, 10);
+  this.paddle = new Paddle(0, 275, 10, 50);
 }
 
 Player.prototype.render = function() {
-  context.fillStyle = "blue";
+  table.fillStyle = "#0000FF";
   this.paddle.render();
 };
 
 Computer.prototype.render = function() {
-  context.fillStyle = "red";
+  table.fillStyle = "#CC3300";
   this.paddle.render();
 };
 
-//ball features
 function Ball(x, y) {
   this.x = x;
   this.y = y;
-  // this.x_speed = 0;
-  // this.y_speed = 3;
   this.radius = 5;
 }
 
-//ball render
 Ball.prototype.render = function() {
-  // context.beginPath();
-  context.arc(this.x, this.y, this.radius, 2 * Math.PI, false);
-  context.fillStyle = "white";
-  context.fill();
+  table.beginPath();
+  table.arc(this.x, this.y, this.radius, 2 * Math.PI, false);
+  table.fillStyle = "#000000";
+  table.fill();
 };
-
 
 var keysDown = {};
 
