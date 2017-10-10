f8d4ac7c65b67d7cc4109acfe3affec5541220b0
Isaac Simon <ijsimon@sbcglobal.net>
2017-10-10 16:38:21 -0500

    I got the ball to bounce off the walls and the paddles correctly

--- scripts/Pong.js
+++ scripts/Pong.js
@@ -4,10 +4,10 @@ window.onload = function() {
 
 window.addEventListener('keydown', function(event){
   if(event.key == "ArrowUp"){
-    player.paddle.move(-2)
+    player.paddle.move(-4)
   }
   else if (event.key == "ArrowDown") {
-      player.paddle.move(2)
+      player.paddle.move(4)
   }
 });
 
@@ -15,6 +15,7 @@ var animate = window.requestAnimationFrame ||
     function(callback) { window.setTimeout(callback, 1000/60) };
 
 function step(){
+    ball.move(player.paddle, computer.paddle);
     render();
     animate(step);
 };
@@ -23,13 +24,10 @@ var ta = document.getElementById('table');
 var width =  1000;
 var height = 600;
 var table = ta.getContext('2d');
-var ta2 = document.getElementById('table2');
-var width =  1000;
-var height = 600;
-var table2 = ta.getContext('2d');
 var player = new Player();
 var computer = new Computer();
-var ball = new Ball(675, 300);
+var ball = new Ball(500, 300);
+var x = true;
 
 var render = function() {
   table.fillStyle = "black"
@@ -55,21 +53,51 @@ Paddle.prototype.render = function() {
 Paddle.prototype.move = function(y) {
   if(this.y < 0){
     this.y = 0
-    console.log(this.y)
-    console.log(this.height)
   }
   else if(this.y > 549){
     this.y = 549;
-    console.log(this.y)
-    console.log(this.height)
   }
   else{
     this.ySpeed = y
     this.y += y;
-    console.log(this.y)
-    console.log(this.height)
   }
 }
+
+Ball.prototype.move = function(pPaddle, cPaddle){
+  this.x += this.xSpeed;
+  this.y += this.ySpeed;
+  var top_x = this.x - 5;
+  var top_y = this.y - 5;
+  var bottom_x = this.x + 5;
+  var bottom_y = this.y + 5;
+
+  if(top_y < 0){
+    this.ySpeed = -this.ySpeed;
+  }
+  else if(bottom_y > 599)
+    this.ySpeed = -this.ySpeed;
+
+  if(top_x > 500) {
+    if (pPaddle.x < top_x + 10 &&
+        pPaddle.x + pPaddle.width > top_x &&
+        pPaddle.y < top_y + 10 &&
+        pPaddle.height + pPaddle.y > top_y){  // hit the player's paddle
+          this.xSpeed = -4;
+          this.ySpeed += (pPaddle.ySpeed / 2);
+          this.x += this.xSpeed;
+        }
+   }
+else {
+  if (cPaddle.x < top_x + 10 &&
+      cPaddle.x + cPaddle.width > top_x &&
+      cPaddle.y < top_y + 10 &&
+      cPaddle.height + cPaddle.y > top_y){   // hit the computer's paddle
+        this.xSpeed = 4;
+        this.ySpeed += (cPaddle.ySpeed / 2);
+        this.x += this.xSpeed;
+    }
+  }
+};
 
 function Player() {
   this.paddle = new Paddle(990, 275);
@@ -92,12 +120,14 @@ Computer.prototype.render = function() {
 function Ball(x, y) {
   this.x = x;
   this.y = y;
+  this.xSpeed = 4;
+  this.ySpeed = 0;
   this.radius = 5;
 }
 
 Ball.prototype.render = function() {
   table.beginPath();
   table.arc(this.x, this.y, this.radius, 2 * Math.PI, false);
-  table.fillStyle = "#000000";
+  table.fillStyle = "white";
   table.fill();
 };
